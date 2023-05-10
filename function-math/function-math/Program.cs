c36aa6ed2ce876cd912283738ceb3c0dbeb4c5fe
Cosalpino <mikael.juillet@cpnv.ch>
2023-05-10 13:34:32 +0200

    wip

--- function-math/function-math/Program.cs
+++ function-math/function-math/Program.cs
@@ -9,11 +9,23 @@ float B = 1;
 float E  = 0;
 
 
+if (f(A)* f(B) > 0 )
+{
+    Console.WriteLine(f(A) * f(B));
+}
+else
+{
+    Console.WriteLine(f(A) * f(B));
+}
+
 
 static float AverageIntervals(float a, float b)
 {
    return (a + b) / 2;
 }
 
-
+static float f(float x)
+{
+   return (float)(Math.Pow(x, 5) - Math.Pow(x, 2) + 3);
+}
 
