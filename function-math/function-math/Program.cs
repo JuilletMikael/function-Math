62e62a2a9f22adc5e4b1c11bf7dac01c424c4066
Cosalpino <mikael.juillet@cpnv.ch>
2023-05-10 10:41:39 +0200

    yaaaaaaaaaaa !

---
+++ function-math/function-math/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Hello, World!");
+
+float A  = 0;
+float B = 1;
+float E  = 0;
+
+
+
+static float AverageIntervals(float a, float b)
+{
+   return (a + b) / 2;
+}
+
+
+
