40d751fb5eae2c81c1bced861bfc6c186a98e32a
Cosalpino <mikael.juillet@cpnv.ch>
2023-06-01 12:14:15 +0200

    finish

--- function-math/aire/FunctionAreaCalculator.cs
+++ function-math/aire/FunctionAreaCalculator.cs
@@ -7,35 +7,50 @@ using System.Threading.Tasks.Dataflow;
 class FunctionAreaCalculator
 {
     public static double A = 0;
-    public static double B = 4;
-    public static double N = 100;
-    public static double S;
-    public static double E;
+    public static double B = 0;
+    public static double N = 0;
 
-    //contenu principal du code
     static void Main()
     {
+        askUser();
+
         double total = 0;
         for (int i = 0; i < N; i++)
         {
-            total += f(i);
+            total += averageIntervals(i);
         }
+
+        Console.WriteLine("L'aire est de " + total);
     }
 
     static double f(double x)
     {
-        return Math.Pow(x, 2) + 2;
+        return Math.Pow(-x, 2) + 36;
     }
+
 
     static double averageIntervals(int i)
     {
-        return calculateD() * f(i) - f(i - 1);
+        double x = A + i * calculateD();
+        return calculateD() * (f(x) + f(x + calculateD())) / 2;
     }
 
     static double calculateD()
     {
-        return B - A / N;
+        return (B - A) / N;
     }
 
-    
+    static void askUser()
+    {
+        Console.WriteLine("-x^2 + 36 [-6 ;6]");
+
+        Console.WriteLine("Quel est votre intervalle négatif ?");
+        double.TryParse(Console.ReadLine(), out A);
+
+        Console.WriteLine("Quel est votre intervalle positif ?");
+        double.TryParse(Console.ReadLine(), out B);
+
+        Console.WriteLine("Quel est N ?");
+        double.TryParse(Console.ReadLine(), out N);
+    }
 }
